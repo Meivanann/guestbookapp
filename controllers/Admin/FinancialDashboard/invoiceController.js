80b71dd92878f338a1a22010adbf5e4afd37f4aa
sriharihara <sriharihara@vbideas.com>
2020-06-13 17:38:41 +0530

    no message

--- controllers/Admin/FinancialDashboard/invoiceController.js
+++ controllers/Admin/FinancialDashboard/invoiceController.js
@@ -3,11 +3,7 @@ const pdfInvoice = require('pdf-invoice');
 // const PDFDocument = require("pdfkit");
 // const fs = require("fs");
 const fs = require('fs')
-const path = require('path')
-const utils = require('util')
-const puppeteer = require('puppeteer')
-const hb = require('handlebars')
-const readFile = utils.promisify(fs.readFile)
+
 
 module.exports = {
     getAllInvoices: (req,res) => {
