717f4106060632794ca1eba4c9aa0d817755cb9a
meivannan <meivannan@rgmobiles.com>
2020-09-04 10:44:22 +0530

    new connection string

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 var mysql      = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host     : 'psatrans-db.caljkao8crbq.us-east-2.rds.amazonaws.com',
+//     user     : 'admin',
+//     password : 'Psatrans#2020',
+//     database : 'psa',
+//     multipleStatements : true
+//   });
+
+
 var connection = mysql.createConnection({
     host     : 'psatrans-db.caljkao8crbq.us-east-2.rds.amazonaws.com',
     user     : 'admin',
     password : 'Psatrans#2020',
-    database : 'psa',
+    database : 'psa_staging',
     multipleStatements : true
   });
 
@@ -12,7 +21,7 @@ var connection = mysql.createConnection({
 //       host     : 'localhost',
 //       port     : '3306',
 //       user     : 'root',
-//       password : 'root',
+//       password : '',
 //       database : 'psa',
 //       multipleStatements : true
 //     });
