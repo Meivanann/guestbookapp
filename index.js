417c75c09f26c6b8435cfedbe426649c2a76120e
meivannan <meivannan@rgmobiles.com>
2020-09-25 21:47:02 +0530

    cron job changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var bodyParser=require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var consignmentcontroller=require('./controllers/Admin/operationalDashboard/consignmentController')
-
+var CronJob = require('cron').CronJob;
+var moment=require('moment')
 const {
     PORT = 8011,
     NODE_ENV = 'developement',
@@ -52,26 +53,30 @@ var connection = require('./config');
 
 
 
-var CronJob = require('cron').CronJob;
+//var CronJob = require('cron').CronJob;
 //var job = new CronJob('59 59 23 * * *', function() {
     var job = new CronJob('59 59 23 * * *', function() {
+        
     //cron job for moving the consignments which is in out for delivery back to cosignments
-    let consignmentQuery = "SELECT * FROM consignment as cd , out_for_delivery as c where cd.status = 'out for delivery' and cd.is_approved = 1 and c.is_done=0;"
+    let consignmentQuery = "SELECT * FROM consignment as cd inner join out_for_delivery as c on c.cn_no=cd.cn_no where cd.status = 'out for delivery' and cd.is_approved = 1 and c.is_done=0  "
+     console.log(consignmentQuery)
     connection.query(consignmentQuery, (err, rows) => {
+        
         if(err){
             console.log(err);
            console.log("Error in some Query");
         } else if (rows.length == 0 ){
            console.log("no Results Found");
         } else {
-            console.log(rows)
+            
+             
             Object.keys(rows).forEach(function(key) {
                 var row = rows[key];
                 let status;
                 let today = new Date();
-                console.log();
+                 
                 let regionQuery = "SELECT * FROM region WHERE destination_code = ? ;"
-                connection.query(regionQuery,row.destination_code, (regerr, regrows) => {
+                connection.query(regionQuery,row.destination_code, async(regerr, regrows) => {
                     if(err){
                         console.log(regerr);
                     }else { 
@@ -83,22 +88,41 @@ var CronJob = require('cron').CronJob;
                             status = "assign to hq";
                         }
              
-                        if(row.expiry_date != null && today >= row.expiry_date){
+                        if(row.expiry_date != null && moment(today).format('YYYY-MM-DD') >= moment(row.expiry_date).format('YYYY-MM-DD')){
+                            
                             let updateConsignmentQuery = "update consignment set status = ?  where cn_no = ?";
                             let consignment_data = [ status, row.cn_no ];
                             let delete_tracking = "delete from out_for_delivery where cn_no = ?;"
-
-                            connection.query(updateConsignmentQuery, consignment_data, (err,rows) => {
+                            console.log('sjsk',today,row.expiry_date)
+                            console.log('fine',consignment_data)
+                          await connection.query(updateConsignmentQuery, [ status, row.cn_no ], async(err,rows) => {
+                                console.log('s',rows)
                                 if(err){
                                     console.log(err)
                                 } else {
                                     console.log("updated sucessfully");
                                 }
                             });
-                            connection.query(delete_tracking, row.cn_no, (err,rows) => {
+                           await  connection.query(delete_tracking, row.cn_no, async(err,rows) => {
                                 if(err){
                                     console.log(err)
                                 } else {
+                                    var tracking_data2 = {
+                                        "cn_no": row.cn_no,
+                                        "status":'CONSIGMENT_EXPIRED',
+                                        "datetime": today,
+                                        isold:1
+                                    }
+                                    
+                                   await connection.query('INSERT INTO tracking SET ?', tracking_data2, (err,rows) => {
+                                        if(err){
+                                            console.log(err);
+                                        } else {
+                                            console.log(rows)
+                                             
+                                        }
+                                    })
+
                                     console.log("Deleted sucessfully");
                                 }
                             })
@@ -118,9 +142,9 @@ var CronJob = require('cron').CronJob;
 job.start();
 
 
-var CronJob = require('cron').CronJob;
+
 //var job = new CronJob('59 59 23 * * *', function() {
-    var trackingjob = new CronJob('0 */1 * * * *', function() {
+    var trackingjob = new CronJob('0 */5 * * * *', function() {
     //cron job for moving the consignments which is in out for delivery back to cosignments
     consignmentcontroller.trackingcronjob()
 }, null, true, 'Asia/Singapore');
