5042cb4ff8957480084cfb7aec51495277886fb4
meivannan <meivannanjayalakshim@gmail.com>
2021-01-29 22:19:23 +0530

    live update link

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 var mongoose      = require('mongoose');
-//Set up default mongoose connection
-var mongoDB = 'mongodb://localhost:27017/guestbookingdb';
+//Set up database link 
+var mongoDB = 'mongodb://10.0.0.240:27017/guestbookingdb'
+//var mongoDB = 'mongodb://localhost:27017/guestbookingdb';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
+
 
  //Get the default connection
 var db = mongoose.connection;
-var connection=''
+ 
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
